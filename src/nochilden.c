950afd1766ea24a1e7ecae1f3c6275d57c2945dc
maxice8 <thinkabit.ukim@gmail.com>
2018-06-29 12:25:00 -0300

    src/: format.

--- src/nochilden.c
+++ src/nochilden.c
@@ -3,25 +3,32 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
-  (void) argc;
+int main(int argc, char *argv[])
+{
+    (void) argc;
 
-  prctl(PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0, 0);
+    prctl(PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0, 0);
 
-  pid_t p;
-  switch ((p = fork())) {
-    case -1: return 255;
-    case  0: execvp(argv[1], argv+1); _exit(127);
-  }
+    pid_t p;
+    switch ((p = fork())) {
+    case -1:
+	return 255;
+    case 0:
+	execvp(argv[1], argv + 1);
+	_exit(127);
+    }
 
-  // try to be a good parent
-  int status;
-  do { waitpid(p, &status, 0); } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    // try to be a good parent
+    int status;
+    do {
+	waitpid(p, &status, 0);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
 
-  // slaughter those fucking kids
-  siginfo_t s;
-  while (waitid(P_ALL, 0, &s, WEXITED|WSTOPPED|WCONTINUED|WNOHANG) != -1)
-    kill(s.si_pid, 9);
+    // slaughter those fucking kids
+    siginfo_t s;
+    while (waitid(P_ALL, 0, &s, WEXITED | WSTOPPED | WCONTINUED | WNOHANG)
+	   != -1)
+	kill(s.si_pid, 9);
 
-  return WEXITSTATUS(status);
+    return WEXITSTATUS(status);
 }
