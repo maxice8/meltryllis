634e3f790ea61c69339394bdd72fb5cd51110f65
Leo <thinkabit.ukim@gmail.com>
2019-07-01 05:52:16 -0300

    fix running ssh

--- etc/fish/conf.d/config.fish
+++ etc/fish/conf.d/config.fish
@@ -1,5 +1,7 @@
 set -x GPG_TTY (tty)
 
-if not set -q SSH_AGENT_PID
+if not pgrep -x ssh-agent -u (id -u)
 	eval (ssh-agent -c)
+	set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+	set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
