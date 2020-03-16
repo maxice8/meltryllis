11aa189587d7b98b3c9d7cecd7c87864b6dccedc
Leo <thinkabit.ukim@gmail.com>
2020-03-16 17:58:23 -0300

    rewrite with luacheck

--- bin/alt-tab-send.lua
+++ bin/alt-tab-send.lua
@@ -1,9 +1,9 @@
 #!/usr/bin/lua5.3
-socket = require 'socket'
+local socket = require 'socket'
 socket.unix = require 'socket.unix'
 
-s = assert(socket.unix())
-m = arg[1] or 'next\n'
+local s = assert(socket.unix())
+local m = arg[1] or 'next\n'
 
 assert(s:connect(os.getenv("SESSION_DIR").."/alt-tab-daemon"))
 assert(s:send(m))
