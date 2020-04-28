90e7e005955b8ba8954a4310629357b9c85b7a3f
Leo <thinkabit.ukim@gmail.com>
2020-04-28 17:03:43 -0300

    remove sway element

--- bin/alt-tab-send.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/lua5.3
-local socket = require 'socket'
-socket.unix = require 'socket.unix'
-
-local s = assert(socket.unix())
-local m = arg[1] or 'next\n'
-
-assert(s:connect(os.getenv("SESSION_DIR").."/alt-tab-daemon"))
-assert(s:send(m))
