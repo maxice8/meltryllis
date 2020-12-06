01387e065820a7ff11b8e6cffb24a8bc2db1fcf1
Leo <thinkabit.ukim@gmail.com>
2020-12-06 17:47:45 -0300

    memes

--- .config/fish/functions/gst.fish
+++ .config/fish/functions/gst.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gst --wraps='g st' --description 'alias gst g st'
-  g st $argv;
+function gst --wraps='git st' --description 'alias gst git st'
+  git st $argv;
 end
