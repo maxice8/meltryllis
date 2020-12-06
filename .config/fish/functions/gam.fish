b07e841ee0f7eac61a8d96a3497e7fac5e77fabf
Leo <thinkabit.ukim@gmail.com>
2020-12-06 18:14:46 -0300

    and even more aliases

---
+++ .config/fish/functions/gam.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gam --wraps='git gam' --description 'alias gam git gam'
+  git gam $argv;
+end
---
+++ .config/fish/functions/prch.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function prch --wraps='git prch' --description 'alias prch git prch'
+  git prch $argv;
+end
