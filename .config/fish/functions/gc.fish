d67bd6cd9382c57c8ca24f327454640ae48beb7d
Leo <thinkabit.ukim@gmail.com>
2020-12-06 17:40:51 -0300

    fish aliases

---
+++ .config/fish/functions/gc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gc --wraps='git c' --description 'alias gc git c'
+  git c $argv;
+end
---
+++ .config/fish/functions/gst.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gst --wraps='g st' --description 'alias gst g st'
+  g st $argv;
+end
