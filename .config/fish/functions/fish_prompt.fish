f8558a898e07affd51458faadf7fe18a2ff48e8c
Leo <thinkabit.ukim@gmail.com>
2020-12-17 13:41:04 -0300

    look at all this fancyness :D

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,17 @@
 function fish_prompt
-  set_color white
-  echo -n "───── "
-  set_color white
+    set_color green
+    if test "$SSH_CLIENT"
+        if test (echo -n "$SSH_CLIENT" | string split ' ' | tail -1) = 2222
+            echo -n "[ssh::toolbox] "
+        else if test (echo -n "$SSH_CLIENT" | string split ' ' | tail -1) = 22
+            echo -n "[ssh::host] "
+        else
+            echo -n "[ssh::"(echo -n $SSH_CLIENT | string split ' ' | tail -1)"] "
+        end
+    else
+        test -n "$TOOLBOX_PATH" && echo -n "[container::toolbox] "
+    end
+    set_color white
+    echo -n "───── "
+    set_color white
 end
