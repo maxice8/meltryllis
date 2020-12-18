9ba452bd5b9c120ace8828a0d327dccd7f7d57bd
Leo <thinkabit.ukim@gmail.com>
2020-12-18 12:32:28 -0300

    use fancier colors

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,9 +1,9 @@
 function fish_prompt
     if test "$SSH_CLIENT"
         set_color brwhite; echo -n "["
-        set_color brblue; echo -n "ssh"
+        set_color ff571a; echo -n "ssh"
         set_color brwhite; echo -n "::"
-        set_color bryellow
+        set_color 00b3b3
         if test (echo -n "$SSH_CLIENT" | string split ' ' | tail -1) = 2222
             echo -n "toolbox"
         else if test (echo -n "$SSH_CLIENT" | string split ' ' | tail -1) = 22
@@ -14,9 +14,9 @@ function fish_prompt
         set_color brwhite; echo -n "] "
     else if test -n "$TOOLBOX_PATH"
         set_color brwhite; echo -n "["
-        set_color brblue; echo -n "container"
+        set_color c53bff; echo -n "container"
         set_color brwhite; echo -n "::"
-        set_color bryellow; echo -n "toolbox"
+        set_color 00b3b3; echo -n "toolbox"
         set_color brwhite; echo -n "] "
     end
     set_color brwhite
